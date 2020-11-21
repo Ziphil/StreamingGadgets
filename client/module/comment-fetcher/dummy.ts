1994206d76e8d0bd775b6ce964a6e650202b1f44
Ziphil <yfe23457@nifty.com>
2020-11-21 13:49:44 +0900

    リファクタリング

--- client/module/comment-fetcher/dummy.ts
+++ client/module/comment-fetcher/dummy.ts
@@ -14,22 +14,22 @@ export class DummyCommentFetcher extends CommentFetcher<DummyCommentFetcherConfi
   public async update(): Promise<Array<Comment>> {
     let length = Math.floor(Math.random() * 3);
     let comments = [...Array(length)].map(() => {
-      let author = this.createRandomString(Math.floor(Math.random() * 12));
-      let text = this.createRandomSentence(10, Math.floor(Math.random() * 12) + 3);
+      let author = DummyCommentFetcher.createRandomString(Math.floor(Math.random() * 12));
+      let text = DummyCommentFetcher.createRandomSentence(10, Math.floor(Math.random() * 12) + 3);
       let comment = new Comment(author, text);
       return comment;
     });
     return comments;
   }
 
-  private createRandomString(length: number): string {
+  private static createRandomString(length: number): string {
     let chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     let string = [...Array(length)].map(() => chars[Math.floor(Math.random() * chars.length)]).join("");
     return string;
   }
 
-  private createRandomSentence(maxWordLength: number, wordSize: number): string {
-    let string = [...Array(wordSize)].map(() => this.createRandomString(Math.floor(Math.random() * maxWordLength) + 1)).join(" ");
+  private static createRandomSentence(maxWordLength: number, wordSize: number): string {
+    let string = [...Array(wordSize)].map(() => DummyCommentFetcher.createRandomString(Math.floor(Math.random() * maxWordLength) + 1)).join(" ");
     return string;
   }
 
